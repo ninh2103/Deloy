043e77f5aa27bf4119ebcefd5177809958d488ec
ninh2103 <nguyenducninh210303@gmail.com>
2025-06-12 22:16:38 +0700

    PM2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'Deloy',
-      script: './node_modules/vite/bin/vite.js -- --host',
-      args: 'preview',
+      script: './node_modules/vite/bin/vite.js',
+      args: 'preview --host',
       interpreter: 'node',
       env: {
         NODE_ENV: 'production'
